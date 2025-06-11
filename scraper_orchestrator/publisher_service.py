59bcff81ed6ff8a9832dc93322f5f896958e115c
Trevor Mooney <trevordmooney@gmail.com>
2025-06-10 20:26:39 -0700

    refactor: refactored run_all_scrapers to include a worker queue and limited the number of threads
    Because Selenium is a heavy process, while running these scrapers on normal desktop/laptops we need to keep the thread count low to 5. I commented out the previous version of run_all_scrapers and included a revised version of run_all_scrapers() and a worker() fn as well. It should work logic-wise, but I am getting some stack trace errors related to chrome driver again, so we may need to revisit the UUID challenge

--- scraper_orchestrator/publisher_service.py
+++ scraper_orchestrator/publisher_service.py
@@ -4,6 +4,7 @@ import time
 import logging
 from confluent_kafka import Producer
 from concurrent.futures import ThreadPoolExecutor
+from queue import Queue
 from scrapers import site1_scraper, site2_scraper  # import scraper modules
 
 class Publisher:
@@ -47,13 +48,50 @@ class Publisher:
             with open("scraper_errors.log", "a") as error_log:
                 error_log.write(f"{time.ctime()}: {error_msg}\n")
 
+    # def run_all_scrapers(self):
+    #     # TODO: Test to see if dropping into subprocess is more efficient at
+    #     #       scale given the changes to the Selenium logic's UUIDs to be
+    #     #       unique;; if using ThreadPools need to limit to between 
+    #     #       ***3 - 10
+    #     #       threads at the same time is all we can handle for selenium
+    #     #       Python threads per batch to conserve cpu resources at scale
+        
+    #     with ThreadPoolExecutor(max_workers=len(self.csv_scrapers)) as executor:
+    #         executor.map(self.run_scraper, self.csv_scrapers)
+
+    #     # Send 'done' message
+    #     self.producer.produce(
+    #         topic=self.topic,
+    #         value=json.dumps({"type": "done"}),
+    #         callback=self.delivery_report
+    #     )
+    #     self.producer.flush()
+
+    def worker(self, scraper_queue):
+        while not scraper_queue.empty():
+            scraper = scraper_queue.get()
+            try:
+                self.run_scraper(scraper)
+            except Exception as e:
+                print(f"Scrape failed: {e}")
+            scraper_queue.task_done()
+
     def run_all_scrapers(self):
-        # TODO: Test to see if dropping into subprocess is more efficient at
-        #       scale given the changes to the Selenium logic's UUIDs to be
-        #       unique;; if using ThreadPools need to limit to between 20-50
-        #       Python threads per batch to conserve cpu resources at scale
-        with ThreadPoolExecutor(max_workers=len(self.csv_scrapers)) as executor:
-            executor.map(self.run_scraper, self.csv_scrapers)
+        # Limited the available threads to 5 because Selenium is a heavy process 
+        # that will be used by the majority of our scrapers. Each thread will 
+        # concurrently pull scrape jobs from a shared queue. When we have more computing 
+        # power can increase the number of threads to be run concurrently.
+
+        scraper_queue = Queue()
+
+        for scraper in self.csv_scrapers:
+            scraper_queue.put(scraper)
+
+        with ThreadPoolExecutor(max_workers= 5) as executor:
+            for _ in range(5):
+                executor.submit(self.worker, scraper_queue)
+
+        scraper_queue.join()
 
         # Send 'done' message
         self.producer.produce(
@@ -62,6 +100,7 @@ class Publisher:
             callback=self.delivery_report
         )
         self.producer.flush()
+
 
 if __name__ == "__main__":
     publisher = Publisher()
